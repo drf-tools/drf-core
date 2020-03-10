3131eba187f4b5c6225b5b8abce1280a9af2caec
Huy Tran <huy.tranquoc@asnet.com.vn>
2020-03-10 13:34:22 +0700

    Change readme from rst to md

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
